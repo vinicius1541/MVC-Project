d9a76f20fc94fc2650e18421761e1c552f4ad405
Vinicius Bernardo <vinicius-bernado2011@hotmail.com>
2021-06-22 16:59:33 -0300

    Adding a new route in ClassRoutes.php for the contact page, adding a nav menu, and in the contact page adding a contact form

---
+++ app/View/Contact/Main.php
@@ -0,0 +1,29 @@
+<div class="center">
+    <main class="container">
+        <h2>Contact Form</h2>
+        <form action="">
+            <div class="input-field">
+                <input type="text" name="name" id="name" placeholder=" ">
+                <label class="input-label" for="name">Name</label>
+                <div class="underline"></div>
+            </div>
+            <div class="input-field">
+                <input type="email" name="mail" id="mail" placeholder=" ">
+                <label class="input-label" for="mail">E-mail</label>
+                <div class="underline"></div>
+            </div>
+            <div class="input-field">
+                <input type="text" name="phone" id="phone" placeholder=" ">
+                <label class="input-label" for="phone">Phone</label>
+                <div class="underline"></div>
+            </div>
+            <div class="input-field">
+                <textarea name="message" id="message" cols="30" rows="10"></textarea>
+                <label class="textarea-label" for="message">Message</label>
+                <div class="underline"></div>
+            </div>
+
+            <input type="submit" value="Continue">
+        </form>
+    </main>
+</div>
